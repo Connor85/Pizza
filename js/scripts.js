2e286c49d4a86c183a5df52ef58731928d5c3427
cm <connormccarthy8@gmail.com>
2018-08-24 10:58:26 -0700

    add side order

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 // business logic
 
-function Pizza (size,toppingOne, toppingTwo, toppingThree) {
+function Pizza (size,toppingOne, toppingTwo, toppingThree, sideOrder) {
   this.size = size;
   this.toppingOne = toppingOne;
   this.toppingTwo = toppingTwo;
   this.toppingThree = toppingThree;
+  this.sideOrder = sideOrder
 }
 
 Pizza.prototype.order = function() {
-  return "<p>" + "Your " + this.size + " " + this.toppingOne + " "+ this.toppingTwo + " " + this.toppingThree + " pizza will arrive soon." + "</p>";
+  return "<p>" + "Your " + this.size + " " + this.toppingOne + " "+ this.toppingTwo + " " + this.toppingThree + " pizza " + "and " + this.sideOrder + " will arrive soon." + "</p>";
 }
 
 // user logic
@@ -21,14 +22,16 @@ $(document).ready(function(){
     var inputtedToppingOne = $("#toppingOne").val();
     var inputtedToppingTwo = $("#toppingTwo").val();
     var inputtedToppingThree = $("#toppingThree").val();
+    var inputtedSideOrder = $("#sideOrder").val();
 
-    var newPizza = new Pizza (inputtedSize, inputtedToppingOne, inputtedToppingTwo, inputtedToppingThree);
+    var newPizza = new Pizza (inputtedSize, inputtedToppingOne, inputtedToppingTwo, inputtedToppingThree, inputtedSideOrder);
 
     $("#result").append("<p>" + newPizza.order() + "</p>");
 
     $('#size').val(" ");
     $('#toppingOne').val(" ");
-    $('#toppingTwo').val(" ")
-    $('#toppingThree').val(" ")
+    $('#toppingTwo').val(" ");
+    $('#toppingThree').val(" ");
+    $('#sideOrder').val(" ");
   });
 });
