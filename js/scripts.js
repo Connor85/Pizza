0f7da2973ff592c3be361326871517ec93096932
cm <connormccarthy8@gmail.com>
2018-08-24 11:36:27 -0700

    styling into correct rows

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,17 @@ Pizza.prototype.order = function() {
   return "<p>" + "Your " + this.size + " " + this.toppingOne + " "+ this.toppingTwo + " " + this.toppingThree + " pizza " + "and " + this.sideOrder + " will arrive soon." + "</p>";
 }
 
+var price = 10
+var cost = function (prices){
+  if (size === medium){
+    return prices + 5
+  } else if (size === large){
+    return prices + 10
+  } else {
+    return prices
+  }
+}
+
 // user logic
 
 $(document).ready(function(){
@@ -26,7 +37,7 @@ $(document).ready(function(){
 
     var newPizza = new Pizza (inputtedSize, inputtedToppingOne, inputtedToppingTwo, inputtedToppingThree, inputtedSideOrder);
 
-    $("#result").append("<p>" + newPizza.order() + "</p>");
+    $("#result").append("<p>" + newPizza.order() + "$" + cost(price) + "</p>");
 
     $('#size').val(" ");
     $('#toppingOne').val(" ");
